955c30920cd446685f2a69079c7d2dc2a06e89cf
Ap-Cristian <cristian.apetroaie@student.unitbv.ro>
2023-12-05 21:33:36 +0200

    Add classes and change project structure

---
+++ sneik/Logic/Models/GameBoad.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Models
+{
+    internal class GameBoad
+    {
+        private Size _size { get; set; }
+        private Obstacle[] _obstacles { get; set; }
+        public Difficulty Difficulty { get; set; }
+        public GameBoad(Size size, Difficulty difficulty)
+        {
+            Difficulty = difficulty;
+            this._size = size;
+
+            // TODO: generate obstacles randomly
+            switch (this.Difficulty)
+            {
+                case Difficulty.EASY:
+                    break;
+                case Difficulty.MEDIUM:
+                    break;
+                case Difficulty.HARD:
+                    break;
+                case Difficulty.VERY_HARD:
+                    break;
+                case Difficulty.NIGHTMARE:
+                    break;
+            }
+
+        }
+    }
+}
---
+++ sneik/Logic/Models/SnakeCell.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Models
+{
+    internal class SnakeCell
+    {
+        public Point Position { get; set; }
+        public Color Color { get; set; }
+        public SnakeCell(Point position, Color color)
+        {
+            Position = position;
+            Color = color;
+        }
+    }
+}

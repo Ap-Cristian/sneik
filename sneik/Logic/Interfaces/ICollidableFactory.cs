39f9124770a45efa6017775372c28140ba10b2a1
Andrei Apostol <113720385+MuzzleGore@users.noreply.github.com>
2024-01-05 15:01:48 +0200

    added collidable factory

---
+++ sneik/Logic/Interfaces/ICollidableFactory.cs
@@ -0,0 +1,9 @@
+﻿using Logic.Models;
+
+namespace Logic.Interfaces
+{
+    public interface ICollidableFactory
+    {
+        ICollidable Create<T>(Point point, Size size, Color color) where T : ICollidable;
+    }
+}
